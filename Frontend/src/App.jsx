d147121e06980ca1a5c05ea4d532cc2b8c91803d
AnkittChauhan <ankittrajput.4@gmail.com>
2025-02-21 20:49:31 +0530

    Added Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='text-red-400'>
+        Hello Tailwind V4
+      </div>
+    </div>
+  )
+}
+
+export default App
